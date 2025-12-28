8280d6fb82f7e90c1d449f9144b16375f71b8b28
Toan Phung <toan.phung@proteo.info>
2025-12-28 08:52:13 +0000

    update

--- plugins/models.py
+++ plugins/models.py
@@ -48,6 +48,7 @@ class Plugin(models.Model):
     submitted_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    tags = models.ManyToManyField('Tag', through='PluginTag', related_name='plugins', blank=True)
 
     def __str__(self):
         return self.name
